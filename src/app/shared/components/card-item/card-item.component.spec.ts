a25c95ce7c974c93f5bca399624205a49def5945
Unknown <ajarac92@gmail.com>
2019-01-24 21:34:37 +0100

    Tests

--- src/app/shared/components/card-item/card-item.component.spec.ts
+++ src/app/shared/components/card-item/card-item.component.spec.ts
@@ -1,4 +1,6 @@
+import { By } from '@angular/platform-browser';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { CardItemComponent } from './card-item.component';
 
@@ -9,7 +11,8 @@ describe('CardItemComponent', () => {
 	beforeEach(
 		async(() => {
 			TestBed.configureTestingModule({
-				declarations: [ CardItemComponent ]
+				declarations: [ CardItemComponent ],
+				schemas: [ NO_ERRORS_SCHEMA ]
 			}).compileComponents();
 		})
 	);
@@ -17,10 +20,83 @@ describe('CardItemComponent', () => {
 	beforeEach(() => {
 		fixture = TestBed.createComponent(CardItemComponent);
 		component = fixture.componentInstance;
+		component.item = {
+			id: '5',
+			name: 'Cuidado con el peatón',
+			brand: 'Liberty',
+			'brand-image': 'brand_liberty.png',
+			kind: 'Coche',
+			'Kind-image': 'kind_car.png',
+			price: '120'
+		};
+		component.listConfig = {
+			headers: [
+				{
+					title: 'Product Name',
+					property: 'name'
+				},
+				{
+					title: 'Insurance Company',
+					property: 'brand'
+				},
+				{
+					title: 'Policy Kind',
+					property: 'kind'
+				},
+				{
+					title: 'Price',
+					property: 'price'
+				}
+			],
+			images: [ 'brand-image' ],
+			actions: [ { icon: 'favorite', getColor: () => () => 'red' } ],
+			title: (item) => `${item.name} - ${item.brand}`
+		};
 		fixture.detectChanges();
 	});
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
+	});
+
+	it('should show title', () => {
+		const title = fixture.debugElement.query(By.css('#title')).nativeElement.textContent;
+
+		expect(title).toBe('Cuidado con el peatón - Liberty');
+	});
+
+	it('should create list of headers', () => {
+		const headers = fixture.debugElement.queryAll(By.css('.row'));
+
+		expect(headers.length).toBe(4);
+	});
+
+	it('should show header and our property', () => {
+		const headers = fixture.debugElement.queryAll(By.css('.row'));
+
+		const header = headers[0].query(By.css('label')).nativeElement.textContent;
+		const property = headers[0].query(By.css('span')).nativeElement.textContent;
+
+		expect(header).toBe('Product Name');
+		expect(property).toBe('Cuidado con el peatón');
+	});
+
+	it('should emit click event when click icon', () => {
+		const icon = fixture.debugElement.query(By.css('#icon_favorite'));
+
+		component.clickAction.subscribe((data: { element: any; action: string }) => {
+			expect(data.element).toEqual({
+				id: '5',
+				name: 'Cuidado con el peatón',
+				brand: 'Liberty',
+				'brand-image': 'brand_liberty.png',
+				kind: 'Coche',
+				'Kind-image': 'kind_car.png',
+				price: '120'
+			});
+			expect(data.action).toBe('favorite');
+		});
+
+		icon.triggerEventHandler('click', 'favorite');
 	});
 });
--- src/app/shared/components/table/table.component.spec.ts
+++ src/app/shared/components/table/table.component.spec.ts
@@ -1,102 +1,57 @@
 import { TableComponent } from './table.component';
 import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { MatTableModule } from '@angular/material';
+import { MatTableModule, MatTableDataSource } from '@angular/material';
 
 xdescribe('TableComponent', () => {
 	let component: TableComponent<any>;
-
-	describe('TableComponent Plain', () => {
-		it('should set dataList in datasource on change lifecycle', () => {
-			component = new TableComponent<any>();
-			component.dataList = [
-				{
-					id: '1',
-					name: 'Casa mia!',
-					brand: 'Generali',
-					'brand-image': 'brand_generali.png',
-					kind: 'Hogar',
-					'Kind-image': 'kind_home.png',
-					price: '300'
-				},
-				{
-					id: '2',
-					name: 'No eres imortal',
-					brand: 'Mapfre',
-					'brand-image': 'brand_mapfre.png',
-					kind: 'Vida',
-					'Kind-image': 'kind_life.png',
-					price: '100'
-				},
-				{
-					id: '3',
-					name: 'Panacea',
-					brand: 'Zurich',
-					'brand-image': 'brand_zurich.png',
-					kind: 'Viaje',
-					'Kind-image': 'kind_travel.png',
-					price: '50'
-				},
-				{
-					id: '4',
-					name: '<mis_datos>',
-					brand: 'AXA',
-					'brand-image': 'brand_axa.png',
-					kind: 'cybersecurity',
-					'Kind-image': 'kind_cibersecurity.png',
-					price: '1010'
-				},
-				{
-					id: '5',
-					name: 'Cuidado con el peatón',
-					brand: 'Liberty',
-					'brand-image': 'brand_liberty.png',
-					kind: 'Coche',
-					'Kind-image': 'kind_car.png',
-					price: '120'
-				}
-			];
-
-			component.ngOnChanges();
-
-			expect(component.dataSource.data.length).toBe(5);
-		});
-
-		it('should build displayedColumns by only headers onInit lifecycle', () => {
-			component = new TableComponent<any>();
-
-			component.headers = [
-				{
-					title: 'Product Name',
-					property: 'name'
-				},
-				{
-					title: 'Insurance Company',
-					property: 'brand'
-				},
-				{
-					title: 'Policy Kind',
-					property: 'kind'
-				},
-				{
-					title: 'Price',
-					property: 'price'
-				}
-			];
-
-			component.ngOnInit();
-
-			expect(component.displayedColumns.length).toBe(4);
-		});
-
-		it('should apply filter to datasource filter trim to lowercase', () => {
-			component = new TableComponent<any>();
-
-			component.applyFilter(' TestIng Filter  ');
-
-			expect(component.dataSource.filter).toBe('testing filter');
-		});
-	});
+	const dataList = [
+		{
+			id: '1',
+			name: 'Casa mia!',
+			brand: 'Generali',
+			'brand-image': 'brand_generali.png',
+			kind: 'Hogar',
+			'Kind-image': 'kind_home.png',
+			price: '300'
+		},
+		{
+			id: '2',
+			name: 'No eres imortal',
+			brand: 'Mapfre',
+			'brand-image': 'brand_mapfre.png',
+			kind: 'Vida',
+			'Kind-image': 'kind_life.png',
+			price: '100'
+		},
+		{
+			id: '3',
+			name: 'Panacea',
+			brand: 'Zurich',
+			'brand-image': 'brand_zurich.png',
+			kind: 'Viaje',
+			'Kind-image': 'kind_travel.png',
+			price: '50'
+		},
+		{
+			id: '4',
+			name: '<mis_datos>',
+			brand: 'AXA',
+			'brand-image': 'brand_axa.png',
+			kind: 'cybersecurity',
+			'Kind-image': 'kind_cibersecurity.png',
+			price: '1010'
+		},
+		{
+			id: '5',
+			name: 'Cuidado con el peatón',
+			brand: 'Liberty',
+			'brand-image': 'brand_liberty.png',
+			kind: 'Coche',
+			'Kind-image': 'kind_car.png',
+			price: '120'
+		}
+	];
 
 	describe('TableComponent TestBed', () => {
 		let fixture: ComponentFixture<TableComponent<any>>;
@@ -114,73 +69,29 @@ xdescribe('TableComponent', () => {
 		beforeEach(() => {
 			fixture = TestBed.createComponent(TableComponent);
 			component = fixture.componentInstance;
-			component.dataList = [
-				{
-					id: '1',
-					name: 'Casa mia!',
-					brand: 'Generali',
-					'brand-image': 'brand_generali.png',
-					kind: 'Hogar',
-					'Kind-image': 'kind_home.png',
-					price: '300'
-				},
-				{
-					id: '2',
-					name: 'No eres imortal',
-					brand: 'Mapfre',
-					'brand-image': 'brand_mapfre.png',
-					kind: 'Vida',
-					'Kind-image': 'kind_life.png',
-					price: '100'
-				},
-				{
-					id: '3',
-					name: 'Panacea',
-					brand: 'Zurich',
-					'brand-image': 'brand_zurich.png',
-					kind: 'Viaje',
-					'Kind-image': 'kind_travel.png',
-					price: '50'
-				},
-				{
-					id: '4',
-					name: '<mis_datos>',
-					brand: 'AXA',
-					'brand-image': 'brand_axa.png',
-					kind: 'cybersecurity',
-					'Kind-image': 'kind_cibersecurity.png',
-					price: '1010'
-				},
-				{
-					id: '5',
-					name: 'Cuidado con el peatón',
-					brand: 'Liberty',
-					'brand-image': 'brand_liberty.png',
-					kind: 'Coche',
-					'Kind-image': 'kind_car.png',
-					price: '120'
-				}
-			];
-			component.headers = [
-				{
-					title: 'Product Name',
-					property: 'name'
-				},
-				{
-					title: 'Insurance Company',
-					property: 'brand'
-				},
-				{
-					title: 'Policy Kind',
-					property: 'kind'
-				},
-				{
-					title: 'Price',
-					property: 'price'
-				}
-			];
-
-			component.actions = [ { icon: 'favorite', getColor: () => () => 'red' } ];
+			component.dataSource = new MatTableDataSource(dataList);
+			component.listConfig = {
+				headers: [
+					{
+						title: 'Product Name',
+						property: 'name'
+					},
+					{
+						title: 'Insurance Company',
+						property: 'brand'
+					},
+					{
+						title: 'Policy Kind',
+						property: 'kind'
+					},
+					{
+						title: 'Price',
+						property: 'price'
+					}
+				],
+				images: [ 'brand-image' ],
+				actions: [ { icon: 'favorite', getColor: () => () => 'red' } ]
+			};
 		});
 
 		it('should create', () => {
